22a391522f45041a60528d0ae1bb269e40ffc7b6
ImAzimm <aabdulazim758@gmail.com>
2026-03-01 07:23:34 +0800

    Fixed config in server wo make sure  can read .env file. The problem occurs previously because in server.js, dotenv.config() is called after the imports. cloudinary.js tries to access process.env at the module level (during import), but at that point, dotenv.config() hasn't been called yet. Meanwhile, db.js doesn't access process.env until connectDB() is called (line 15 in server.js), which is after dotenv.config()

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,13 +1,8 @@
 import cloudinary from 'cloudinary';
 
 cloudinary.v2.config({
-  // cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  // api_key: process.env.CLOUDINARY_API_KEY,
-  // api_secret: process.env.CLOUDINARY_API_SECRET,
-  // TO DO: Check why cloudinary upload is not working with env variables, hardcoding for now
-  cloud_name: "dfqddfqma",
-  api_key: "936257765873736",
-  api_secret: "dA9gWLTU7RknbnfZ0mFB97vpnlI",
-});
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,});
 
 export default cloudinary.v2;
