b38aba3c8559b1d0dffd63b0152e36f9a1ecf2a9
ImAzimm <aabdulazim758@gmail.com>
2026-03-05 23:19:08 +0800

    setup cludinary configuration in adminr routes to make sure credentials are configured before attempting uploads so that env files can be read in correct order

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,8 +1,15 @@
 import cloudinary from 'cloudinary';
 
-cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,});
+// Configure cloudinary with environment variables
+const configureCloudinary = () => {
+  cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+};
+
+// Configure on import and when needed
+configureCloudinary();
 
 export default cloudinary.v2;
